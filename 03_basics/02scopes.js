64b7b5edd90a8ac28692746b602239563a6e466b
Priyansu_proves <90149156+priyansu12@users.noreply.github.com>
2023-11-21 22:10:30 +0530

    iife

---
+++ 03_basics/02scopes.js
@@ -0,0 +1,36 @@
+function one() {
+    const userName = "Priyansu"
+
+    function two() {
+        const website = "linkedin";
+        console.log(userName, website);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if (true ){
+    const userName = "Priyansu"
+    if (userName == "Priyansu") {
+        const  website = "test"
+        console.log("Hello", userName + website)
+    }
+}
+
+// console.log(userName);
+
+
+///***************************imp************************* *///
+function addOne(num) {
+    return num + 1
+}
+
+console.log(addOne(5));
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(5));
