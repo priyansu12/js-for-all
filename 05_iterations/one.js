aa68388ab6676d160790eeb21252f90212d836be
Priyansu_proves <90149156+priyansu12@users.noreply.github.com>
2023-11-25 11:50:49 +0530

    loops1

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -2,4 +2,12 @@
 //   // code to be executed
 // }
 
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if (element === 5) {
+//         console.log("Element is 5");
+//     }
+//     console.log(element);
+// }
+
 
---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let index = 0;
+
+while (index < 10) {
+    console.log(index);
+    index++;
+} // 0 1 2 3 4 5 6 7 8 9
+
+let arr = ['flash', 'audio', 'video',]
+let arrindex = 0;
+while (arrindex < arr.length) {
+    console.log(arr[arrindex]);
+    arrindex++;
+} // flash audio video
+
+let score = 1;
+
+do {
+    console.log(score);
+} while (score <= 10);
