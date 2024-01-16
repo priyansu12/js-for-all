042b7c3c4db85c8a777e0847ef9454b9d934cce8
Priyansu_proves <90149156+priyansu12@users.noreply.github.com>
2024-01-17 00:12:40 +0530

    DOM part1

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -5,3 +5,10 @@ programming.forEach((value, index) => {
     console.log(index, ':- ', value);
 })
 
+const greetings = "Hello World"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
